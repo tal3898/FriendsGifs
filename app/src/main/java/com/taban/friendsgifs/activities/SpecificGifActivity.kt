2fa2d61eec945f5b40723154810a87d961c7e0e8
Tal <tal.bny3@gmail.com>
2019-11-14 18:06:50 +0200

    added body to share button

--- app/src/main/java/com/taban/friendsgifs/activities/SpecificGifActivity.kt
+++ app/src/main/java/com/taban/friendsgifs/activities/SpecificGifActivity.kt
@@ -1,8 +1,10 @@
 package com.taban.friendsgifs.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.taban.friendsgifs.views.GifImageView
 import com.taban.friendsgifs.utility.Globals.Companion.GIF_ID_INTENT_PARAMETER
 import com.taban.friendsgifs.utility.Globals.Companion.LOG_TAG
@@ -18,5 +20,14 @@ class SpecificGifActivity : AppCompatActivity() {
         Log.i(LOG_TAG, "created with gif id " + gifResourceId)
         var gifImageView = findViewById(R.id.gif) as GifImageView
         gifImageView.setGifResource(gifResourceId)
+    }
+
+    fun onShareButtonClick(view : View) {
+        val sharingIntent = Intent(Intent.ACTION_SEND)
+        sharingIntent.type = "text/plain"
+        val shareBody = "Here is the share content body"
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here")
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody)
+        startActivity(Intent.createChooser(sharingIntent, "Share via"))
     }
 }
